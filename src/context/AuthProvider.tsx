df5dc32ebfb2e9dad4f8d8a3bdbea44807b51194
Edson Lima <edsonteles343@gmail.com>
2024-12-22 01:29:46 -0300

    fix

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, useContext } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { jwtDecode } from "jwt-decode";
 import { useRouter } from "expo-router";
 import { loginApi } from "../service/users";
